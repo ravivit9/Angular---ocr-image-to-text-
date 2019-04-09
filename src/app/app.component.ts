d09e4292dd0b918a526b7f7b1f2ef4b607bbd108
veysel sebu <veysel.sebu.23@gmail.com>
2019-04-09 14:45:39 +0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {Tesseract} from 'tesseract.ts';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,62 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-ocr';
+  file = null;
+  sonuc: any;
+  progres: any = '';
+  tesseractSonuc: any;
+  sonucVarMi = false ;
+
+  constructor() {
+    this.sonucVarMi = false;
+  }
+
+  fileUpload(file) {
+    console.log(file.target.files[0]);
+    this.file = file.target.files[0];
+  }
+
+  calistir() {
+    this.sonucVarMi = false;
+    this.sonuc = ' ';
+    this.progres = 0;
+    Tesseract
+      .recognize(this.file)
+      .progress(pro => {
+        if (pro.status === 'recognizing text') {
+          this.progres = (pro.progress * 100);
+        }
+      })
+      .then((res: any) => {
+        this.sonuc = res.html;
+        this.tesseractSonuc = res;
+        console.log(res);
+      })
+      .catch(console.error);
+  }
+
+  sonucDondur(degisken: number) {
+    switch (degisken) {
+      case 1 :
+        this.sonucVarMi = false;
+        this.sonuc = this.tesseractSonuc.text;
+        break;
+      case 2:
+        this.sonucVarMi = true;
+        this.sonuc = this.tesseractSonuc.words;
+        break;
+      case 3:
+        this.sonucVarMi = true;
+        this.sonuc = this.tesseractSonuc.paragraphs;
+        break;
+      case 4:
+        this.sonucVarMi = true;
+        this.sonuc = this.tesseractSonuc.symbols;
+        break;
+      case 5:
+        this.sonucVarMi = true;
+        this.sonuc = this.tesseractSonuc.lines;
+        break;
+    }
+  }
 }
